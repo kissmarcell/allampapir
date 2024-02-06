af5416c6dd08612878147fc20e94871bfd578857
Marcell Kiss <marcell@kmarcell.com>
2024-02-07 00:39:14 +0100

    Refactored retrieval
    - Renamed variables
    - Do not make extra check for existing folders
    - Removed storing of exact retrieval time
    - Changed folder structure

--- download.py
+++ download.py
@@ -8,11 +8,12 @@ PAPER_TYPES_REQUEST: str = "https://www.allampapir.hu/api/network_rate/m/get_pap
 PAPER_DATA_REQUEST: str = "https://www.allampapir.hu/api/network_rate/m/get_prices/{}"
 DATA_FOLDER_NAME: str = "data"
 
-for key in requests.get(PAPER_TYPES_REQUEST).json()["data"].keys():
-    folder_path: str = os.path.join(os.getcwd(), DATA_FOLDER_NAME, key)
-    if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
-        os.makedirs(folder_path)
-    file_path: str = os.path.join(folder_path, f"{date.today()}_{int(time.time())}.json")
-    with open(file_path, "w") as f:
-        paper: dict = requests.get(PAPER_DATA_REQUEST.format(key)).json()
-        json.dump(paper["data"]["data"], f)
+for bond_type in requests.get(PAPER_TYPES_REQUEST).json()["data"].keys():
+    all_bonds_by_type: dict = requests.get(PAPER_DATA_REQUEST.format(bond_type)).json()["data"]["data"]
+    for bond in all_bonds_by_type:
+        folder_path: str = os.path.join(os.getcwd(), DATA_FOLDER_NAME, bond_type, bond["longName"])
+        os.makedirs(folder_path, exist_ok=True)
+        file_path: str = os.path.join(folder_path, f"{date.today()}.json")
+        if not os.path.exists(file_path):
+            with open(file_path, "w") as f:
+                json.dump(bond, f)
