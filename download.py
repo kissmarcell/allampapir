90175d6e2d986d2d713adab9c90914643cf48229
Marcell Kiss <marcell@kmarcell.com>
2025-12-07 14:23:51 +0100

    Updated request method

--- download.py
+++ download.py
@@ -7,8 +7,8 @@ PAPER_TYPES_REQUEST: str = "https://www.allampapir.hu/api/network_rate/m/get_pap
 PAPER_DATA_REQUEST: str = "https://www.allampapir.hu/api/network_rate/m/get_prices/{}"
 DATA_FOLDER_NAME: str = "data"
 
-for bond_type in requests.get(PAPER_TYPES_REQUEST).json()["data"].keys():
-    all_bonds_by_type: dict = requests.get(PAPER_DATA_REQUEST.format(bond_type)).json()["data"]["data"]
+for bond_type in requests.post(PAPER_TYPES_REQUEST).json()["data"].keys():
+    all_bonds_by_type: dict = requests.post(PAPER_DATA_REQUEST.format(bond_type)).json()["data"]["data"]
     for bond in all_bonds_by_type:
         folder_path: str = os.path.join(os.getcwd(), DATA_FOLDER_NAME, bond_type, bond["longName"])
         os.makedirs(folder_path, exist_ok=True)
